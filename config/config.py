17ceb07c7ae8d55d6e53f7f4e7c4341ed1252b82
cheungbh <534660436@qq.com>
2020-03-13 20:17:20 +0800

    add more structure

--- config/config.py
+++ config/config.py
@@ -23,15 +23,27 @@ pose_batch = 80
 TCN_structure = {1:[[6, 6, 6, 6], 5, 2],
                  # [channel_size, kernel_size]
                  2: [[3, 4, 5, 6], 5, 4],
-                 3: [[6, 6, 6, 6], 7, 2]
+                 3: [[6, 6, 6, 6], 7, 2],
+                 4: [[12, 12, 12, 12], 7, 2],
+                 5: [[8, 16, 8, 16], 7, 2],
+                 6: [[6, 6, 6, 6], 7, 4],
+                 7: [[6, 6, 6, 6], 7, 8],
+                 8: [[6, 6, 6, 6], 7, 1],
                  }
 
 
 # ConvLSTM structure
-ConvLSTM_structure = {1:[[128, 64, 64, 32, 32], (7, 7), False],
+ConvLSTM_structure = {1: [[128, 64, 64, 32, 32], (7, 7), False],
                       # [hidden_channels, kernel_size, attention]
-                      2:[[128, 64, 64, 32, 32], (7, 7), True],
-                      3:[[128, 64, 64, 64, 32], (5, 5), True],
+                      2: [[128, 64, 64, 32, 32], (7, 7), True],
+                      3: [[128, 64, 64, 64, 32], (5, 5), True],
+                      4: [[128, 64, 32], (3, 3), True],
+                      5: [[128, 64, 32, 32, 32], (5, 5), True],
+                      6: [[128, 64, 32], (3, 3), False],
+                      7: [[128, 128, 128, 64, 32], (7, 7), False],
+                      8: [[128, 128, 128, 64, 32], (7, 7), True],
+                      9: [[128, 128, 128, 64, 32], (3, 3), True],
+                      10: [[128, 32], (3, 3), False],
                       }
 
 
@@ -39,6 +51,12 @@ ConvLSTM_structure = {1:[[128, 64, 64, 32, 32], (7, 7), False],
 LSTM_structure = {1: [[128, 128], [64, 16]],
                   2: [[128, 128, 128], [64, 32, 8]],
                   3: [[128, 128], [16]],
+                  4: [[128, 64], [64, 8]],
+                  5: [[128, 128, 128, 128], [64, 16]],
+                  6: [[128, 128, 128], [64, 16]],
+                  7: [[128, 128], [64, 32, 16, 8]],
+                  8: [[128, 64], [64, 64, 16]],
+                  9: [[128, 128, 128], [64, 32, 16, 8]],
                   }
 
 # Video process
@@ -66,12 +84,12 @@ merge_comment = "Inpute test: swim, test100, test101, all samples, 30f, 10s"
 
 # Auto training config
 data_path = '5_input/input1/'
-networks = ["TCN", "LSTM", "ConvLSTM"]
+networks = ["LSTM"]
 
 epoch_ls = {"LSTM": [200, 500, 1000],
             "TCN": [100, 200, 500],
             "ConvLSTM": [100, 200]}
-dropout_ls = {"LSTM": [0.2, 0.3],
+dropout_ls = {"LSTM": [0.2],
               "TCN": [0.05, 0.1],
               "ConvLSTM": [""]}  # ConvLSTM don't have any dropouts
 lr_ls = {"LSTM": [1e-4],
@@ -79,7 +97,7 @@ lr_ls = {"LSTM": [1e-4],
          "ConvLSTM": [1e-4]}
 structure_ls = {
     "ConvLSTM": [1, 2, 3],
-    "LSTM": [1, 2, 3],
+    "LSTM": [1],
     "TCN": [1, 2, 3]
 }
 
@@ -90,7 +108,7 @@ log_interval = 5
 training_labels = {0:"swim", 1:"drown"}
 
 data_info = "The data comes from input1, all datas, label is {swim, drown}, 30 frames, 10 steps"
-out_dest = "6_network/net1"
+out_dest = "6_network/test"
 
 
 
