e585893988a3bf7511a4fade031cd2971d5668da
cheungbh <534660436@qq.com>
2020-03-21 23:27:48 +0800

    less point

--- tools/check_size.py
+++ tools/check_size.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-txt_path = '../5_input/input_test/data.txt'
+txt_path = '../4_data/cut_point/30frames/10steps/test100/test100_0.txt'
 
 file_matrix = np.loadtxt(txt_path)
 print("The file has {} rows".format(file_matrix.shape[0]))
