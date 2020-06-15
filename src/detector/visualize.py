5ae873bb4d60bcfc997ca3938e76f7c604618b24
SAIL-LAB155 <saillab155@gmail.com>
2020-06-15 15:32:41 +0800

    int

--- src/detector/visualize.py
+++ src/detector/visualize.py
@@ -7,5 +7,5 @@ class BBoxVisualizer(object):
 
     def visualize(self, bboxes, img):
         for bbox in bboxes:
-            img = cv2.rectangle(img, (bbox[0], bbox[1]), (bbox[2], bbox[3]), self.color, 8)
+            img = cv2.rectangle(img, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), self.color, 8)
         return img
