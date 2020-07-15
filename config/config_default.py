7b5ec11e02e938c1234335a5ed125123d22bd9e4
cheungbh <534660436@qq.com>
2020-07-15 12:54:42 +0800

    new cfg

--- config/config_default.py
+++ config/config_default.py
@@ -2,7 +2,7 @@ import torch
 
 device = "cuda:0"
 print("Using {}".format(device))
-size = (540, 360)
+size = (720, 540)
 
 # For yolo
 confidence = 0.05
@@ -10,10 +10,8 @@ num_classes = 80
 nms_thresh = 0.33
 input_size = 416
 
-# yolo_cfg = "src/yolo/cfg/yolov3-swim-416.cfg"
-# yolo_weights = 'weights/yolo/yolov3-swim-416_40000.weights'
-yolo_cfg = "config/yolo_cfg/yolov3-spp-1cls.cfg"
-yolo_weight = 'weights/yolo/best.weights'
+yolo_cfg = "config/yolo_cfg/yolov3-swim-416.cfg"
+yolo_weight = 'weights/yolo/yolov3-swim-416_40000.weights'
 
 # For SPPE
 input_height = 320
@@ -33,7 +31,6 @@ body_parts_ls = ["Nose", "Left eye", "Right eye", "Left ear", "Right ear", "Left
 body_part = {value: idx for idx, value in enumerate(body_parts_ls)}
 pose_cls = 17
 
-
 # TCN structure
 TCN_structure = {1:[[6, 6, 6, 6], 5, 2],
                  # [channel_size, kernel_size, dilation]
@@ -43,8 +40,9 @@ TCN_structure = {1:[[6, 6, 6, 6], 5, 2],
                  5: [[8, 16, 8, 16], 7, 2],
                  6: [[6, 6, 6, 6], 7, 4],
                  7: [[6, 6, 6, 6], 7, 8],
-                 8: [[6, 6, 6, 6], 7, 1]
+                 8: [[6, 6, 6, 6], 7, 1],
                  }
+TCN_single = True
 
 
 # ConvLSTM structure
@@ -85,21 +83,21 @@ BiLSTM_structure = {1: [64, 2, False],
                     }
 
 
-TCN_single = True
 # Video process
-video_process_class = ["trash"]
+video_process_class = ["test1"]
 save_kps_video = False
-save_kps_img = True
+save_kps_img = False
 save_black_img = False
 save_frame = False
-process_gray = True
+process_gray = False
 
 
 # Coordinate process
-coord_step = 5
-coord_frame = 30
+coord_step = 10
+coord_frame = 20
 coord_process_method = "ordinary"  #Do not change now
-coord_process_class = ["drown_0624", "Stand", "swim_0623"]
+coord_process_class = ["drown", "swim"]
+
 
 
 
@@ -107,28 +105,28 @@ coord_process_class = ["drown_0624", "Stand", "swim_0623"]
 merge_step = 5
 merge_frame = 30
 merge_process_method = "ordinary"
-merge_class = ["drown_0624", "Stand"]
-merge_dest_name = "2_class_30_0707"
-merge_comment = "{}: [drown_0624,Stand], all samples, 30f, 5s, wrong posture has been deleted".format(merge_dest_name)
+merge_class = ["drown", "swim"]
+merge_dest_name = "input4"
+merge_comment = "{}: [drown, swim], all samples, 20f, 10s, wrong posture has been deleted".format(merge_dest_name)
 
 
 # Auto training config
-train_data_path = '5_input/2_class_30_0707'
-out_dest = "6_network/3_class_30_0707"
-data_info = "{}: The data comes from {}, equal data, [drown_0624,Stand], 30 frames, 5 steps".format(out_dest.split("/")[1],
+train_data_path = '5_input/input2/equal'
+out_dest = "6_network/net4"
+data_info = "{}: The data comes from {}, equal data, [drown, swim], 30 frames, 5 steps".format(out_dest.split("/")[1],
                                                                       "/".join(train_data_path.split("/")[1:]))
 network_name = ["ConvLSTM", "ConvGRU", "BiLSTM", "TCN", "LSTM"]
-network_num = [3,4]
+network_num = [0, 1, 2, 3]
 
 
 networks = [network_name[idx] for idx in network_num]
-epoch_ls = {"LSTM": [80],
-            "TCN": [500],
-            "ConvLSTM": [100],
-            "ConvGRU": [100],
-            "BiLSTM": [300], }
-dropout_ls = {"LSTM": [0.05],
-              "TCN": [0.05],
+epoch_ls = {"LSTM": [2],
+            "TCN": [250],
+            "ConvLSTM": [50],
+            "ConvGRU": [80, 150],
+            "BiLSTM": [80, 150], }
+dropout_ls = {"LSTM": [0.2],
+              "TCN": [0.05, 0.1],
               "ConvLSTM": [""],
               "ConvGRU": [""],
               "BiLSTM": [""]
@@ -140,38 +138,49 @@ lr_ls = {"LSTM": [1e-4],
          "BiLSTM": [1e-4], }
 structure_ls = {
     "ConvLSTM": [1, 2, 3],
-    "LSTM": [4,6], #checked [4, 5, 6, 7, 8]
-    "TCN": [1,4,5,6,7], #checked 2
+    "LSTM": [4, 5, 6, 7, 8],
+    "TCN": [1, 2, 3, 4, 5, 6],
     "ConvGRU": [1, 2],
     "BiLSTM": [1, 2],
 }
 
-batch_size = {"LSTM": 32, "TCN": 32, "ConvLSTM": 8, "ConvGRU": 8, "BiLSTM": 128} #{"LSTM": 128, "TCN": 128, "ConvLSTM": 64, "ConvGRU": 128, "BiLSTM": 128}
+batch_size = {"LSTM": 8, "TCN": 8, "ConvLSTM": 8, "ConvGRU": 8, "BiLSTM": 8}
 kps_num = 34
-training_frame = 30
-log_interval = 10
-train_val_ratio = 0.2
-training_labels = {0: "drown", 1: "Stand", 2:"swim" }
+training_frame = 20
+log_interval = 5
+train_val_ratio = 0.4
+training_labels = {0: "drown", 1: "swim" }
 
 
 # Auto labelling config
-label_comment = "label1: 30 frames, 3 classes: (drown,Stand,swim)"
+label_comment = "label1: 30 frames, 2 classes: (swim, drown)"
 label_frame = 30
-label_cls = ["drown_side", "Stand","swim" ]
-label_folder_name = "trash"
-label_main_folder = "7_test/test"
-write_label_info = True
+label_cls = ["drown", "swim", ]
+label_folder_name = "label3"
+label_main_folder = "tmp/write_info"
+write_label_info = False
 
 
 # Auto testing config
-test_model_folder = "6_network/3_class_30_0626_more/model"
-test_video_folder = "7_test/test/video"
-test_label_folder = "7_test/test/trash"
-test_res_file = "8_result/trash/trash.csv"
+test_model_folder = "tmp/net9/model"
+test_video_folder = "tmp/train_v/video"
+test_label_folder = "tmp/train_v/label1"
+test_res_file = "8_result/result4/train_video_res.csv"
 test_write_video = True
 
 test_kps_num = 34
 testing_frame = 30
 
+
 testing_log = "{}: all the datas, swim and drown, 30 frames, 10 steps. Video coming from v_1, and label_1".\
     format(test_res_file.split("/")[1])
+
+
+#visualize
+track_idx = "all"    # If all idx, track_idx = "all"
+track_plot_id = ["all"]   # If all idx, track_plot_id = ["all"]
+assert track_idx == "all" or isinstance(track_idx, int)
+
+plot_bbox = True
+plot_kps = True
+plot_id = True
