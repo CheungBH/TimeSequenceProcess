0d74dd661652462af44960969af03ffbc3bbd43b
cheungbh <534660436@qq.com>
2020-08-07 21:36:54 +0800

    pose option

--- config/config_default.py
+++ config/config_default.py
@@ -184,3 +184,18 @@ assert track_idx == "all" or isinstance(track_idx, int)
 plot_bbox = True
 plot_kps = True
 plot_id = True
+
+
+import os
+pose_option = os.path.join("/".join(pose_weight.replace("\\", "/").split("/")[:-1]), "option.pkl")
+if os.path.exists(pose_option):
+    info = torch.load(pose_option)
+    pose_backbone = info.backbone
+    pose_cfg = info.struct
+    pose_cls = info.kps
+    DUC_idx = info.DUC
+
+    output_height = info.outputResH
+    output_width = info.outputResW
+    input_height = info.inputResH
+    input_width = info.inputResW
